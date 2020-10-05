22e21be22228218ee4bbadb1daff9dfd895bbde2
phnga <ngapham3108@gmail.com>
2020-10-05 22:30:07 +0700

    S4.1: View and Template - View

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url, include
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<qid>[0-9]+)/$', views.detail, name= 'detail'),
+    url(r'^(?P<qid>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<qid>[0-9]+)/vote/$', views.vote, name = 'vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(req):
-    respond = HttpResponse()
-    respond.write("<h1>this's my site</h1>")
-    return respond
+    response = HttpResponse()
+    response.write("<h1>this's my site</h1>")
+    return response
+
+def detail(req,qid):
+    return HttpResponse("You're looking at question %s." % qid)
+
+def results(req,qid):
+    return HttpResponse("You're looking at results of the question %s." % qid)
+
+def vote(req,qid):
+    return HttpResponse("You're voting on question %s." % qid)
