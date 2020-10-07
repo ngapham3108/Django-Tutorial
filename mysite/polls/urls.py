47f4f754ad17eb9e05810fb42105d3808886ddce
phnga <ngapham3108@gmail.com>
2020-10-07 14:29:33 +0700

    S4.2: vIEW and Template - Template + 404 + dynamic URL + Namespace

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from . import views
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<qid>[0-9]+)/$', views.detail, name= 'detail'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from  django.template import loader
 # Create your views here.
 def index(req):
-    response = HttpResponse()
-    response.write("<h1>this's my site</h1>")
-    return response
+    latest_quesiton_list = Question.objects.order_by('-pub_date')[0:10]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_quesiton_list
+    }
+    #return HttpResponse(template.render(context,req))
+    return render(req,'polls/index.html', context)
 
 def detail(req,qid):
-    return HttpResponse("You're looking at question %s." % qid)
+    try:
+        question = Question.objects.get(pk=qid)
+        # for choice in question.choice_set.all():
+        #     print(choice.choice_set)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(req,'polls/detail.html',{'question':question})
+
 
 def results(req,qid):
     return HttpResponse("You're looking at results of the question %s." % qid)
